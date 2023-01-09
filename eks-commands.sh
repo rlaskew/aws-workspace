f04da6806596c13edc2de2ea7ab7169fa35b7a5d
rlaskew <richardaskew@yahoo.com>
2023-01-09 12:03:52 -0600

    Update eks-commands.sh

--- eks-commands.sh
+++ eks-commands.sh
@@ -3,3 +3,7 @@
 # install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl create cluster --region us-east-1 -t t3.medium --dry-run
+eksctl create cluster --region us-east-1 -t t3.medium 
+kubectl get nodes
